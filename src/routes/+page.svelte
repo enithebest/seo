8218bd02203d9eafd4c27a833ffe22925609d6f9
enithebest <aenihasmegaj@gmail.com>
2026-03-04 12:38:15 +0100

    new

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,12 @@
 
 <script>
     import background from '$lib/assets/background.jpg';
+    import { title } from 'process';
 </script>
+<svelte:head>
+<title>Car that make you feel better </title>
+<meta name="description" content="Find the perfect toy for your child with our wide selection of high-quality toys. Shop now and bring joy to your little one's life!" />
+</svelte:head>
 
 <header class="h-[500px] bg-cover bg-center flex items-center justify-center" style="background-image: url({background})">
 
