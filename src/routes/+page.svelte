bad2bdb403bb4b7010271c4fbe1efca1e60f8504
enithebest <aenihasmegaj@gmail.com>
2026-03-04 12:29:19 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+
+
+<script>
+    import background from '$lib/assets/background.jpg';
+</script>
+
+<header class="h-[500px] bg-cover bg-center flex items-center justify-center" style="background-image: url({background})">
+
+<h1 class="text-5xl text-white">Toys for your liking </h1>
+
+</header>
+
+<main>
+
+</main>
+
+<footer>
+
+</footer>
