4955f4b7746c64c7ddf4ea875b10be014956f131
enithebest <aenihasmegaj@gmail.com>
2026-03-04 12:50:22 +0100

    new

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,7 @@
 <svelte:head>
 <title>Car that make you feel better </title>
 <meta name="description" content="Find the perfect toy for your child with our wide selection of high-quality toys. Shop now and bring joy to your little one's life!" />
+<meta property="og:title" content="Car that make you feel better " />
 </svelte:head>
 
 <header class="h-[500px] bg-cover bg-center flex items-center justify-center" style="background-image: url({background})">
@@ -18,6 +19,6 @@
 <main>
 
 </main>
-<footer>
-
+<footer class="h-[300px] bg-gray-800 text-white flex items-center justify-center">
+ <p class="text-center text-gray-500 py-4">© 2024 Toy Store. All rights reserved.</p>
 </footer>
