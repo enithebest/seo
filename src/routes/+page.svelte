e8c0b62ba472a43d8544ea31fc6bf39f24df66dc
enithebest <aenihasmegaj@gmail.com>
2026-03-04 12:51:05 +0100

    new

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,8 @@
 <title>Car that make you feel better </title>
 <meta name="description" content="Find the perfect toy for your child with our wide selection of high-quality toys. Shop now and bring joy to your little one's life!" />
 <meta property="og:title" content="Car that make you feel better " />
+<meta property="og:description" content="Find the perfect toy for your child with our wide selection of high-quality toys. Shop now and bring joy to your little one's life!" />
+<meta property="og:image" content="{background}" /> 
 </svelte:head>
 
 <header class="h-[500px] bg-cover bg-center flex items-center justify-center" style="background-image: url({background})">
