e99b3398e3d50cabe14c24657a2daf876f0e23eb
enithebest <aenihasmegaj@gmail.com>
2026-03-04 12:36:18 +0100

    new 3d model

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,18 @@
+
+
 <script>
     import background from '$lib/assets/background.jpg';
 </script>
 
-<header class="h-[500px]" style="background-image: url({background})">
-    <div>123</div>
+<header class="h-[500px] bg-cover bg-center flex items-center justify-center" style="background-image: url({background})">
+
+<h1 class="text-5xl text-white">Toys for your liking </h1>
+
 </header>
 
 <main>
-    <div></div>
-</main>
 
+</main>
 <footer>
-    <div></div>
+
 </footer>
